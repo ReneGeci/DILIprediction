2548fd9552796994592b553a044c6fbd4c66dd61
René Geci <81250995+ReneGeci@users.noreply.github.com>
2025-06-18 12:28:42 +0200

    Initial

---
+++ code + figures/run HTPBK simulations.R
@@ -0,0 +1,111 @@
+# set working directory to code directory
+setwd("C:/Users/User/OneDrive - Uniklinik RWTH Aachen/PhD/R model files/analysis/2024_06_14 Zeynep DILI/data/superdata v2/GitHub Repo/code + figures")
+
+inSilicoCLModelFilesPath = "../model files/In silico clearance"
+inVivoCLModelFilesPath =  "../model files/In vivo clearance (if available)"
+  
+
+# make sure OSP suite and OSP Suite R package are installed
+library(ospsuite)
+library(tidyverse)
+library(writexl)
+library(readxl)
+
+# execute HTPBK Simulations
+for (file in list.files(inSilicoCLModelFilesPath)){
+  
+  sim = loadSimulation(paste0(inSilicoCLModelFilesPath, "/", file))
+  
+  simResults = runSimulation(sim)
+  
+  simResultsPath = paste0("../data/Simulation results/In silico clearance/", gsub("pkml", "csv", file))
+  exportResultsToCSV(simResults, simResultsPath)
+}
+
+
+for (file in list.files(inVivoCLModelFilesPath)){
+  
+  sim = loadSimulation(paste0(inVivoCLModelFilesPath, "/", file))
+  
+  simResults = runSimulation(sim)
+  
+  simResultsPath = paste0("../data/Simulation results/In vivo clearance (if available)/", gsub("pkml", "csv", file))
+  exportResultsToCSV(simResults, simResultsPath)
+}
+
+
+
+# extract Cmax values of each drug
+inSilicoCLResultsPath = "../data/Simulation results/In silico clearance"
+inVivoCLResultsPath =  "../data/Simulation results/In vivo clearance (if available)"
+
+
+
+
+# In Silico
+CmaxSummary = data.frame(compoundID = numeric(), type = character(),
+                         firstCmax = numeric(), lastCmax = numeric(),
+                         stringsAsFactors = F)
+
+for (file in list.files(inSilicoCLResultsPath)){
+  compoundID = as.numeric(str_extract(file, "^[0-9]+"))
+  
+  simResults = read.csv(paste0(inSilicoCLResultsPath, "/", file))
+  
+  firstPlasmaCmax = simResults  %>%
+    filter(Time..min. <= (60*24*1))
+  
+  firstPlasmaCmax = max(firstPlasmaCmax$Organism.PeripheralVenousBlood.average.compound.Plasma..Peripheral.Venous.Blood...µmol.l.)
+  lastPlasmaCmax = max(simResults$Organism.PeripheralVenousBlood.average.compound.Plasma..Peripheral.Venous.Blood...µmol.l.)
+  
+  newRow = data.frame(
+    compoundID = compoundID,
+    type = "InSilico",
+    firstPlasmaCmax = firstPlasmaCmax,
+    lastPlasmaCmax = lastPlasmaCmax,
+    stringsAsFactors = FALSE
+  )
+  
+  CmaxSummary =  rbind(CmaxSummary, newRow)
+}
+write_xlsx(CmaxSummary, paste0(inSilicoCLResultsPath, "/AllCmaxValuesMergedInSilico.xlsx"))
+
+
+
+
+# In Vivo
+CmaxSummary = data.frame(compoundID = numeric(), type = character(),
+                         firstCmax = numeric(), lastCmax = numeric(),
+                         stringsAsFactors = F)
+
+for (file in list.files(inVivoCLResultsPath)){
+  compoundID = as.numeric(str_extract(file, "^[0-9]+"))
+  
+  simResults = read.csv(paste0(inVivoCLResultsPath, "/", file))
+  
+  firstPlasmaCmax = simResults  %>%
+    filter(Time..min. <= (60*24*1))
+  
+  firstPlasmaCmax = max(firstPlasmaCmax$Organism.PeripheralVenousBlood.average.compound.Plasma..Peripheral.Venous.Blood...µmol.l.)
+  lastPlasmaCmax = max(simResults$Organism.PeripheralVenousBlood.average.compound.Plasma..Peripheral.Venous.Blood...µmol.l.)
+  
+  newRow = data.frame(
+    compoundID = compoundID,
+    type = "InVivo",
+    firstPlasmaCmax = firstPlasmaCmax,
+    lastPlasmaCmax = lastPlasmaCmax,
+    stringsAsFactors = FALSE
+  )
+  
+  CmaxSummary =  rbind(CmaxSummary, newRow)
+}
+write_xlsx(CmaxSummary, paste0(inVivoCLResultsPath, "/AllCmaxValuesMergedInVivo.xlsx"))
+
+
+# finally, merge both into a single Cmax file
+InVivoCLCmax = read_xlsx(paste0(inVivoCLResultsPath , "/AllCmaxValuesMergedInVivo.xlsx"))
+InSilicoCLCmax = read_xlsx(paste0(inSilicoCLResultsPath, "/AllCmaxValuesMergedInSilico.xlsx"))
+
+
+mergedDf = rbind(InVivoCLCmax, InSilicoCLCmax)
+write_xlsx(mergedDf, path = "../data/Simulation results/AllCmaxValuesMerged.xlsx")
---
+++ readme.md
@@ -0,0 +1,43 @@
+# Integrating in vivo and in silico approaches enables prediction of drug-induced liver injury
+___
+
+by
+René Geci,
+Ahenk Zeynep Sayin,
+Stephan Schaller,
+Lars Kuepfer
+
+now available here: XXX
+___
+
+## Overview
+
+> The repository contains the following
+> 1. `code + figures`: the code required to reproduce simulation results and figures
+> 2. `data`: the data collected from literature and generated during simulations
+> 3. `model files`: the PBK model files of each drug and the PBBA model
+___
+
+
+## Getting the files
+You can download a copy of all the files in this repository by cloning this repository:
+
+    git clone https://github.com/ReneGeci/DILIprediction.git
+
+or [download a zip archive](https://github.com/ReneGeci/DILIprediction/archive/master.zip).
+
+
+## Investigating the code
+
+To run model simulations, you need to at least install the Open Systems pharmacology suite, its R package and all package dependencies required.
+
+To learn more about the installation of the Open Systems Pharmacology suite (PK-Sim & MoBi) go to their [webpage](https://www.open-systems-pharmacology.org/).
+
+For an explanation how to install the OSP R package, go here [github](https://github.com/open-systems-pharmacology/ospsuite-r)
+
+For documentation and explanation how the OSP R package works, go here [webpage]
+https://www.open-systems-pharmacology.org/OSPSuite-R/).
+
+
+## Funding
+This work was performed in the context of the ONTOX project (https://ontoxproject.eu/) that has received funding from the European Union’s Horizon 2020 Research and Innovation programme under grant agreement No 963845. ONTOX is part of the ASPIS project cluster (https://aspiscluster.eu/). AZS and LK acknowledge financial support by the German Federal Ministry of Education and Research (BMBF), grant number 03LW0304K
